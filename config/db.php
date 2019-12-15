ced15240ed41d8457e7ae9651e8197f38b58dab5
Your Name <you@example.com>
2019-12-15 03:10:58 +0200

    master

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=olimp',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=emoye_olimp',
+    'username' => 'emoye_olimp',
+    'password' => 'l+MS5a_4m6',
     'charset' => 'utf8',
 ];
