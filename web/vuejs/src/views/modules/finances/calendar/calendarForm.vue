31d51f1b209ad8a5e911da41946e8f16067500d4
dmitrii.skriaga <dmitrii.skriaga@ignited.it>
2020-07-15 05:44:32 +0300

    master

--- web/vuejs/src/views/modules/finances/calendar/calendarForm.vue
+++ web/vuejs/src/views/modules/finances/calendar/calendarForm.vue
@@ -2,7 +2,7 @@
   <v-layout row justify-center>
     <v-dialog v-model="showCalendarDialog" persistent max-width="600px">
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+<!--        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>-->
       </template>
       <v-card>
         <v-card-title>
