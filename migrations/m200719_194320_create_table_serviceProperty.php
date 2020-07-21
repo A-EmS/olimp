a4b2ed348901205e9eb3b828076e8c51d87d61d4
dmitrii.skriaga <dmitrii.skriaga@ignited.it>
2020-07-21 23:55:06 +0300

    master

--- migrations/m200719_194320_create_table_serviceProperty.php
+++ migrations/m200719_194320_create_table_serviceProperty.php
@@ -8,7 +8,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
 
     public function up()
     {
-        $this->execute('CREATE TABLE `service_property` (
+        $this->execute('CREATE TABLE IF NOT EXISTS `service_property` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `name` varchar(255) NOT NULL,
               `create_user` int(11) DEFAULT \'0\',
@@ -19,7 +19,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               UNIQUE KEY `name_UNIQUE` (`name`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `service_property_value` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `service_property_value` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `service_id` int(11) NOT NULL,
               `service_property_id` int(11),
@@ -32,7 +32,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               UNIQUE KEY `service_property` (`service_id`,`service_property_id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `finance_documents` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `finance_documents` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `parent_document_id` int(11) NOT NULL,
               `type` int(11) NOT NULL,
@@ -50,7 +50,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               KEY `type` (`type` ASC)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `account_content` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `account_content` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `account_id` int(11) NOT NULL,
               `service_id` int(11) NOT NULL,
@@ -64,7 +64,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `finance_book` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `finance_book` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `account_id` int(11) NOT NULL,
               `service_id` int(11) NOT NULL,
@@ -78,7 +78,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `finance_operation_type` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `finance_operation_type` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `name` varchar(255) NOT NULL,
               `create_user` int(11) DEFAULT \'0\',
@@ -89,7 +89,7 @@ class m200719_194320_create_table_serviceProperty extends Migration
               UNIQUE KEY `name_UNIQUE` (`name`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `finance_calendar` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `finance_calendar` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
               `account_id` int(11) NOT NULL,
               `payment_status_id` int(11) NOT NULL DEFAULT 0,
@@ -102,9 +102,9 @@ class m200719_194320_create_table_serviceProperty extends Migration
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('CREATE TABLE `finance_book` (
+        $this->execute('CREATE TABLE IF NOT EXISTS  `finance_book` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
-              `operation_type_id` int(11) NOT NULL,
+              `operation_id` int(11) NOT NULL,
               `property_1` varchar (1000),
               `property_2` varchar (1000),
               `contractor_id` datetime DEFAULT NULL,
@@ -120,20 +120,21 @@ class m200719_194320_create_table_serviceProperty extends Migration
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8');
 
-        $this->execute('
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'29\', \'servicesProperty\', \'Services Property\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'30\', \'contracts\', \'Contracts\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'31\', \'annexes\', \'Annexes\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'32\', \'acts\', \'Acts\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'33\', \'accounts\', \'Accounts\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'34\', \'financeBook\', \'Finance Book\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'35\', \'financeOperationType\', \'Finance Operation Type\');
-            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'36\', \'financeCalendar\', \'Finance Calendar\');
-        ');
+//        $this->execute('
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'29\', \'servicesProperty\', \'Services Property\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'30\', \'contracts\', \'Contracts\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'31\', \'annexes\', \'Annexes\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'32\', \'acts\', \'Acts\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'33\', \'accounts\', \'Accounts\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'34\', \'financeBook\', \'Finance Book\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'35\', \'financeOperationType\', \'Finance Operation Type\');
+//            INSERT INTO `access_item` (`id`, `name`, `title_alias`) VALUES (\'36\', \'financeCalendar\', \'Finance Calendar\');
+//        ');
     }
 
     public function down()
     {
+
         $this->execute('DROP table service_property');
         $this->execute('DROP table service_property_values');
         $this->execute('DROP table finance_documents');
