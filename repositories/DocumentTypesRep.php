059aee737a4c05428f88a76bdf9e9eea469ec2f5
alastar <dskri@ua.fm>
2021-04-06 23:16:47 +0300

    master

--- repositories/DocumentTypesRep.php
+++ repositories/DocumentTypesRep.php
@@ -11,12 +11,14 @@ class DocumentTypesRep extends DocumentTypes
     const SCENARIO_TYPE_AD_AGREEMENT = 3;
     const SCENARIO_TYPE_ACCOUNT = 4;
     const SCENARIO_TYPE_ACT = 5;
+    const SCENARIO_TYPE_COMMERCIAL_OFFERING = 6;
 
     const SCENARIO_TYPE_CONTRACT_TEXT = 'Contract Scenario';
     const SCENARIO_TYPE_ANNEX_TEXT = 'Annex Scenario';
     const SCENARIO_TYPE_AD_AGREEMENT_TEXT = 'Additional Agreement Scenario';
     const SCENARIO_TYPE_ACCOUNT_TEXT = 'Account Scenario';
     const SCENARIO_TYPE_ACT_TEXT = 'Act Scenario';
+    const SCENARIO_TYPE_COMMERCIAL_OFFERING_TEXT = 'Commercial Offering';
 
     const SCENARIOS = [
         self::SCENARIO_TYPE_CONTRACT => self::SCENARIO_TYPE_CONTRACT_TEXT,
@@ -24,6 +26,7 @@ class DocumentTypesRep extends DocumentTypes
         self::SCENARIO_TYPE_AD_AGREEMENT => self::SCENARIO_TYPE_AD_AGREEMENT_TEXT,
         self::SCENARIO_TYPE_ACCOUNT => self::SCENARIO_TYPE_ACCOUNT_TEXT,
         self::SCENARIO_TYPE_ACT => self::SCENARIO_TYPE_ACT_TEXT,
+        self::SCENARIO_TYPE_COMMERCIAL_OFFERING => self::SCENARIO_TYPE_COMMERCIAL_OFFERING_TEXT,
     ];
 
     const SCENARIOS_IDS = [
@@ -32,6 +35,7 @@ class DocumentTypesRep extends DocumentTypes
         self::SCENARIO_TYPE_AD_AGREEMENT_TEXT => self::SCENARIO_TYPE_AD_AGREEMENT,
         self::SCENARIO_TYPE_ACCOUNT_TEXT => self::SCENARIO_TYPE_ACCOUNT,
         self::SCENARIO_TYPE_ACT_TEXT => self::SCENARIO_TYPE_ACT,
+        self::SCENARIO_TYPE_COMMERCIAL_OFFERING_TEXT => self::SCENARIO_TYPE_COMMERCIAL_OFFERING,
     ];
 
     public static function checkDuplicateByCountryAndName(int $countryId, string $name, $exceptedId = null)
